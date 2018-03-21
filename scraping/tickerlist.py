683fd3003fe740ae81be23430a1210a0dbf79bf3
funkman95 <samfunk375@gmail.com>
2018-03-21 10:58:33 -0500

    make scraping folder

---
+++ scraping/tickerlist.py
@@ -0,0 +1,21 @@
+def make_list():
+    with open('/Users/samfunk/ds/metis/project_mcnulty/datafiles/tickers.txt', 'r') as f:
+        tickers = f.read().split('\n')
+
+    for ind, tick in enumerate(tickers):
+        if '-' in tick:
+            tickers[ind] = tick.replace('-', '')
+
+    tickers.remove('GOOG')
+    tickers.remove('FOXA')
+    tickers.remove('DXC')
+    tickers.remove('BHGE')
+    tickers.remove('FTI')
+    tickers.remove('BHF')
+    tickers.remove('DISCK')
+    tickers.remove('UAA')
+    tickers.remove('NWS')
+    tickers.remove('IVZ')
+    tickers.pop()
+
+    return tickers
